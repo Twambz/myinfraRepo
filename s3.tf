8cbd9b064cb9fe87eda32bce70c757860bce73f9
Twambz <132775904+Twambz@users.noreply.github.com>
2023-06-24 13:55:36 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,19 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
+  bucket = "my-s3bucket-2023"
+
+  # Other bucket configurations
+
+  tags = {
+    "environment" = "DEV"
+    "terraform"   = "true"
   }
-  
-  tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+
+  versioning {
+    enabled    = true
+    mfa_delete = false
+  }
 }
